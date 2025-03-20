a195d83450e32d7c22153633828386456046d955
Lea Henri <lea-henri@live.fr>
2025-03-20 17:37:37 +0100

    controller method create

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,4 +1,18 @@
 class ParticipationsController < ApplicationController
+
+  def create
+    @mission = Mission.find(params[:mission_id])
+    @participation = Participation.new
+    @participation.mission = @mission
+    @participation.user = current_user
+    @participation.status = "pending"
+    if @participation.save
+      redirect_to dashboard_path
+    else
+      redirect "mission/show", status: :unprocessable_entity
+    end
+  end
+
   def accept
     @paticipation = Participation.find(params[:id])
     @participation.update(status: "accepted")
