aabf167d3fafe0d302d0345badd5db7d1d4bbea3
Lea Henri <lea-henri@live.fr>
2025-03-24 14:32:16 +0100

    form déroulant

--- app/javascript/controllers/form_expand_controller.js
+++ app/javascript/controllers/form_expand_controller.js
@@ -1,13 +1,26 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["selectContainer"];
+  static targets = ["selectField"];
 
   expand(event) {
-    event.currentTarget.parentElement.classList.add("visible");
+    event.currentTarget.style.transition = "margin-bottom 0.3s"
+    // vérifier la margin botton de event.currentTarget
+    const mb = event.currentTarget.style.marginBottom
+    // si c'est 0 alors la liste n'est pas déroulée
+    if (mb == "") {
+      // on doit calculer la taille du margin-bottom
+      const size = event.currentTarget.children.length
+      event.currentTarget.style.marginBottom = `${size*29}px`
+    }
+    else {
+      event.currentTarget.style.marginBottom = ""
+    }
+    // si ce n'est pas 0 la liste est déroulée
+    // on lui attribut margin-bottom 0
   }
 
   collapse(event) {
-    event.currentTarget.parentElement.classList.remove("visible");
+    event.currentTarget.style.marginBottom = ""
   }
 }
